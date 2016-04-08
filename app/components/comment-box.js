ab674f608051a8e01d200559b3c017317439df8a
Eric Kelly <heroiceric@gmail.com>
2016-04-08 15:30:55 -0700

    Reactive state - fetch comments from server

--- app/components/comment-box.js
+++ app/components/comment-box.js
@@ -1,11 +1,25 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
+const {
+  Component,
+  get,
+  inject,
+  set
+} = Ember;
+
+const { service } = inject;
 
 export default Component.extend({
+  ajax: service(),
+
   init() {
     this._super(...arguments);
 
     this.data = [];
+
+    get(this, 'ajax')
+      .request(get(this, 'url'))
+      .then((comments) => set(this, 'data', comments))
+      .catch((error) => console.error(error));
   }
 });
